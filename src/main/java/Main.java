60b6a47d218f1192e0ca1f3a39ba89789f788c38
Lucas Vilela <lucas.owned@hotmail.com>
2019-01-13 01:37:55 -0200

    Modified Sabios routine.
    Signed-off-by: Lucas Vilela <lucas.owned@hotmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,6 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
-import java.util.ArrayList;
 
 public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
